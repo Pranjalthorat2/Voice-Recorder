e5864f0d4dfd24541303a7aec50085347bd793f4
Pranjalthorat2 <142740555+Pranjalthorat2@users.noreply.github.com>
2023-12-16 20:45:43 +0530

    Add files via upload

---
+++ Voice Recorder/api.php
@@ -0,0 +1,21 @@
+<?php 
+
+if(count($_FILES)>0){
+
+	$folder = "uploads/";
+	if(!file_exists($folder)){
+		mkdir($folder,0777,true);
+	}
+
+	foreach ($_FILES as $key => $FILE) {
+		# code...
+		if($FILE['error'] == 0){
+			
+			$filename = $FILE['tmp_name'];
+			$destination = $folder . $FILE['name'];
+
+			move_uploaded_file($filename, $destination);	
+		}
+	}
+}
+
